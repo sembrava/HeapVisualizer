fc627f28ec54f382f8b737230b7267a2b89903c3
sembrava <martin.sembrava@gmail.com>
2024-09-18 21:42:36 +0200

    Add more utility functions

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -2,7 +2,7 @@
 
 #include "utils.h"
 
-std::vector<std::string> Utils::split(const std::string &string, char delimiter)
+std::vector<std::string> Utils::split(const std::string& string, char delimiter)
 {
     std::vector<std::string> tokens;
     std::istringstream tokenStream(string);
@@ -14,4 +14,27 @@ std::vector<std::string> Utils::split(const std::string &string, char delimiter)
     }
 
     return tokens;
+}
+
+constexpr int Utils::getParent(const int child) noexcept
+{
+    return (child - 1) / 2;
+}
+
+
+constexpr int Utils::getLeftChild(const int parent) noexcept
+{
+    return parent * 2 + 1;
+}
+
+constexpr int Utils::getRightChild(const int parent) noexcept
+{
+    return parent * 2 + 2;
+}
+
+void Utils::swap(int &x, int &y) noexcept
+{
+    const int temp = x;
+    x = y;
+    y = temp;
 }
