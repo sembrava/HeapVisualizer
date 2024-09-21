432f434eeb6dd3fea11970dbb7719d5f5e262bd5
sembrava <martin.sembrava@gmail.com>
2024-09-21 19:20:22 +0200

    Refactor utils

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -9,32 +9,14 @@ std::vector<std::string> Utils::split(const std::string& string, char delimiter)
     std::string token;
 
     while (std::getline(tokenStream, token, delimiter))
-    {
         tokens.push_back(token);
-    }
 
     return tokens;
 }
 
-constexpr int Utils::getParent(const int child) noexcept
+void Utils::swap(int& first, int& second)
 {
-    return (child - 1) / 2;
-}
-
-
-constexpr int Utils::getLeftChild(const int parent) noexcept
-{
-    return parent * 2 + 1;
-}
-
-constexpr int Utils::getRightChild(const int parent) noexcept
-{
-    return parent * 2 + 2;
-}
-
-void Utils::swap(int &x, int &y) noexcept
-{
-    const int temp = x;
-    x = y;
-    y = temp;
+    const int temp = first;
+    first = second;
+    second = temp;
 }
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -7,6 +7,23 @@
 namespace Utils
 {
     std::vector<std::string> split(const std::string& string, char delimiter);
+
+    [[nodiscard]] constexpr int getParent(const int child)
+    {
+        return (child - 1) / 2;
+    }
+
+    [[nodiscard]] constexpr int getLeftChild(const int parent)
+    {
+        return parent * 2 + 1;
+    }
+
+    [[nodiscard]] constexpr int getRightChild(const int parent)
+    {
+        return parent * 2 + 2;
+    }
+
+    void swap(int& first, int& second);
 }
 
 #endif // UTILS_H
