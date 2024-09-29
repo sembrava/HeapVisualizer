4f65e5be5c547829763240ea26788b7a7986516d
sembrava <martin.sembrava@gmail.com>
2024-09-29 14:49:19 +0200

    Add modelmanager as context property

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "view/modelmanager.h"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    ModelManager modelManager;
+    engine.rootContext()->setContextProperty("modelManager", &modelManager);
+
     const QUrl url(QStringLiteral("qrc:/HeapVisualizer/src/view/ui/Main.qml"));
+
     QObject::connect(
         &engine,
         &QQmlApplicationEngine::objectCreationFailed,
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
+
     engine.load(url);
 
     return app.exec();
